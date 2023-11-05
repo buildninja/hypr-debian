80f164880c6111dcae38f0c9fd438f38f1f43376
Steve <10862416+buildninja@users.noreply.github.com>
2023-11-04 18:00:16 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -9,9 +9,14 @@ dependencies=(
     dunst
     waybar
     tmux
+    unzip
+    swaybg
+    tldr
+    pavucontrol
 )
 
 for PKG1 in "${dependencies[@]}"; do
   sudo apt-get install -y  "$PKG1"
 done
 
+tldr -u
