c7ecd5920bf301393a4ee5f5dc33ba655f0f7beb
Steve <10862416+buildninja@users.noreply.github.com>
2024-03-16 16:40:09 -0700

    Update command.sh
    working build dependencies

--- command.sh
+++ command.sh
@@ -1,1 +1,7 @@
 sudo apt-get install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev xdg-desktop-portal-wlr libtomlplusplus3
+
+sudo apt-get install -y libcairo2-dev libpango1.0-dev libgbm-dev libvulkan-dev linux-headers-$(uname -r)
+
+sudo apt build-dep wlroots
+
+#these worked when following the hyprland wiki build instructions for ubuntu
